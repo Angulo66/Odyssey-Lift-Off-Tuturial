43cfcb5ee9596cc0cce17887ed6f2e158c1a2026
helder <angulohelder2021@gmail.com>
2021-04-01 19:44:56 -0700

    finishing first part of lift off

---
+++ client/src/pages/tracks.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Layout } from '../components';
+import { gql } from '@apollo/client';
+
+/** TRACKS query to retrieve all tracks */
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const Tracks = () => {
+  return <Layout grid> </Layout>;
+};
+
+export default Tracks;
